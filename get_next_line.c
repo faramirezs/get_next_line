31465c76619a4489704b4a9c86d92d0876b7c39c
Alejandro Ramirez <alramire@c3a1c4.42berlin.de>
2024-07-04 20:10:26 +0200

    mod con repo

--- get_next_line.c
+++ get_next_line.c
@@ -42,6 +42,8 @@ char *extract_line (char * big_buffer)
 		return(NULL);
 	}
 	else  */
+	if(!big_buffer)
+		return(NULL);
 	if (ft_strchr(big_buffer, '\n') != NULL)
 	{
 		nl_char = ft_strchr(big_buffer, '\n');
@@ -67,8 +69,33 @@ char *extract_line (char * big_buffer)
 	return(line);
 }
 
-char *obtain_remaining (char *big_buffer, char *line){
+char *obtain_remaining (char *big_buffer)
+{
+	int i;
+	int j;
+	char *tmp;
 
+	i = 0;
+	while (big_buffer[i] != '\0' && big_buffer[i] != '\n')
+		i++;
+	if (big_buffer[i] == '\0')
+	{
+		free(big_buffer);
+		return (NULL);
+	}
+	tmp = (char *)ft_calloc(ft_strlen(big_buffer) - i + 1, sizeof(char));
+	if (!tmp)
+	{
+		free(big_buffer);
+		return (NULL);
+	}
+	i++;
+	j = 0;
+	while (big_buffer[i] != '\0')
+		tmp[j++] = big_buffer[i++];
+	tmp[j] = '\0';
+	free(big_buffer);
+	return (tmp);
 /* 	if (big_buffer && line)
 	{
 		printf("si big_buffer:%s and si line: %s\n", big_buffer, line);
@@ -82,7 +109,7 @@ char *obtain_remaining (char *big_buffer, char *line){
 		//printf("Return equal cero from obtain_remaining");
         return(0);
     } */
-	if (ft_strcmp(line, big_buffer) == 0)
+/* 	if (ft_strcmp(line, big_buffer) == 0)
 	{
 		printf("Check if line and big_buffer are equal\n");
 		return NULL;
@@ -92,29 +119,33 @@ char *obtain_remaining (char *big_buffer, char *line){
         big_buffer = ft_substr_remaining(big_buffer, ft_strlen(line) + 1, ft_strlen(big_buffer));
         printf("Remaining after removing line, this is big_buffer: %s\n", big_buffer);
         return(big_buffer);
-    }
+    } */
 }
 
 char *read_file (char *big_buffer, int fd) {
 	int r_bytes;
 	char * buffer;
 
+	if(!big_buffer)
+		big_buffer = ft_calloc(1, sizeof(char));
 	buffer = ft_calloc(BUFFER_SIZE + 1, sizeof(char));
 	if (buffer == NULL) {
 		printf("Memory allocation failed\n");
 		return(NULL);
 	}
 	r_bytes = 1;
-	while(r_bytes > 0){
+	while(!ft_strchr(big_buffer, '\n') && r_bytes != 0)
+	{
 		r_bytes = read(fd, buffer, BUFFER_SIZE);
 		if (r_bytes == -1)
 		{
 			printf("Read error");
 			free(big_buffer);
-			return (free(buffer), NULL);
+			free(buffer);
+			return (NULL);
 
 		}
-		else if (r_bytes == 0)
+/* 		else if (r_bytes == 0)
 		{
 			printf("End of file!\n");
 			close(fd);
@@ -122,14 +153,14 @@ char *read_file (char *big_buffer, int fd) {
 			free(buffer);
 			return (0);
 			//break;
-		}
+		} */
 		// Is this null terminated str really neccesary, it is not enough with the callocated from the above? Yes it is neccesary because the allocation is biiger than the actual string until the nl
 		//fprintf(stderr, "I'm here after break\n");
 		buffer[r_bytes] = '\0';
 		// Antes del return de Append big_buffer queda liberado y asignado al malloc de strjoin
 		big_buffer = append_buffer(big_buffer, buffer);
 		//printf("big_buffer inside read loop is: %s\n", big_buffer);
-		if (big_buffer == NULL)
+/* 		if (big_buffer == NULL)
 		{
 			printf("Memory allocation failed\n");
 			return (free(buffer), NULL);
@@ -138,7 +169,7 @@ char *read_file (char *big_buffer, int fd) {
 		{
 			//printf("nl found and loop broke in read_file()\n");
 			break;
-		}
+		} */
 	}
 	//printf("Big Buffer outside loop is: %s\n", big_buffer);
 	free(buffer);
@@ -155,23 +186,25 @@ char * get_next_line(int fd) {
 
 	if(fd < 0 || BUFFER_SIZE <= 0 || read(fd, 0, 0) < 0 )
 	{
-		printf("Failed to open file or BUFFER_SIZE is too small\n");
+		if(big_buffer != NULL)
+			free(big_buffer);
+		//printf("Failed to open file or BUFFER_SIZE is too small\n");
 		return(NULL);
 	}
 	if (!big_buffer)
 	{
-		big_buffer = ft_calloc(1, sizeof(char));
+		return (NULL);
 	}
-	if (!ft_strchr(big_buffer, '\n'))
+	/* if (!ft_strchr(big_buffer, '\n'))
 	{
 		big_buffer = read_file(big_buffer, fd);
 	}
 	if (!big_buffer)
 	{
 		return(free(big_buffer), NULL);
-	}
+	} */
 	line = extract_line(big_buffer);
 	//printf("Obtain remaining 1\n");
-	big_buffer = obtain_remaining(big_buffer, line);
+	big_buffer = obtain_remaining(big_buffer);
  	return (line);
 }
